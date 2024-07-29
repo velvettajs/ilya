70790bc05dfa800ef9880d31c2a5b39800c908c9
osint <osintaxis@gmail.com>
2024-07-29 13:27:24 -0500

    removing giphy features

--- src/api/github-webhook.ts
+++ src/api/github-webhook.ts
@@ -1,11 +1,8 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { eventConfig } from "../config/events";
-import { GiphyFetch } from "@giphy/js-fetch-api";
 import "dotenv/config";
 import axios from "axios";
 
-const GIPHY_API_KEY: string = process.env.GIPHY_API_KEY as string;
-const gf = new GiphyFetch(GIPHY_API_KEY);
 const webhookUrl: string = process.env.WEBHOOK_URL as string;
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
@@ -15,7 +12,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const authorAvatarUrl = payload.sender?.avatar_url;
   const repoName = payload.repo?.name;
   const eventDetails = eventConfig[githubEvent] || {
-    color: 0x7e7ece,
     description: "An unknown event occurred.",
     getDetails: (payload) => ({
       title: `Event in ${repoName}`,
@@ -23,8 +19,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       url: payload.repository?.html_url,
     }),
   };
-  const { data: gifs } = await gf.search("aesthetic cat", { limit: 35 });
-  const gif = gifs[Math.floor(Math.random() * gifs.length)];
   const { title, description, fields, url } = eventDetails.getDetails(payload);
   const embed = {
     author: {
@@ -34,13 +28,11 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     title,
     description,
     fields,
+    color: 0x7e7ece,
     url,
     timestamp: new Date().toISOString(),
     footer: {
       text: `Event type: ${githubEvent}`,
-    },
-    thumbnail: {
-      url: gif.images.original.url,
     },
   };
   const message = {
